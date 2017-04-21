1c91b69223017e33d3a4b7b2da90cfd29d3c6529
Cassie Musolf <cassiemusolf@gmail.com>
2017-04-21 09:41:15 -0700

    add details files for the products.

--- src/GummiBearKingdom/Controllers/ProductsController.cs
+++ src/GummiBearKingdom/Controllers/ProductsController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummiBearKingdom.Models;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GummiBearKingdom.Controllers
 {
@@ -17,5 +16,11 @@ namespace GummiBearKingdom.Controllers
         {
             return View(db.Products.ToList());
         }
+  
+    public IActionResult Details(int id)
+    {
+        var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+        return View(thisProduct);
     }
+  }
 }
