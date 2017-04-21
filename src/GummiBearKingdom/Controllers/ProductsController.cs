b7e58439704450e73da760dabbc1ebf48161fcbe
Cassie Musolf <cassiemusolf@gmail.com>
2017-04-21 09:57:58 -0700

    add method to controller to edit the product, add edit view page.

--- src/GummiBearKingdom/Controllers/ProductsController.cs
+++ src/GummiBearKingdom/Controllers/ProductsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummiBearKingdom.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace GummiBearKingdom.Controllers
 {
@@ -30,10 +30,24 @@ namespace GummiBearKingdom.Controllers
 
     [HttpPost]
     public IActionResult Create(Product product)
-        {
-            db.Products.Add(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-  }
+    {
+        db.Products.Add(product);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+        return View(thisProduct);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Product product)
+    {
+        db.Entry(product).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    }
 }
